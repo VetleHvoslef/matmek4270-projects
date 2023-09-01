eea48f3269f094e7c88aab9d190abda41580eb69
Vetle Henrik Hvoslef <vetlehh@uio.no>
2023-09-01 13:36:40 +0200

    The test for mesh_function passes

--- projects/python/mesh_function.py
+++ projects/python/mesh_function.py
@@ -2,13 +2,23 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    pass
+    mesh = np.zeros(len(t))
+    for i in range(len(t)):
+        mesh[i] = f(t[i])
+    return mesh
 
 def func(t):
-    pass
+    if 0 <= t <= 3:
+        return np.exp(-t)
+    elif 3 < t <= 4:
+        return np.exp(-3 * t)
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
     f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
     fun = mesh_function(func, t)
     assert np.allclose(fun, f)
+
+# t = np.array([1, 2, 3, 4])
+# f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
+# fun = mesh_function(func, t)
