e07c34c221ff49373c2cd95136e65295be50207a
Vetle Henrik Hvoslef <vetlehh@uio.no>
2023-09-04 09:39:49 +0200

    The test for differentiate.py passes now, was some indicies problems

--- projects/python/differentiate.py
+++ projects/python/differentiate.py
@@ -1,11 +1,23 @@
 import numpy as np
 
+# N_t er en mindre enn totalt antall punkter
 
 def differentiate(u, dt):
-    pass
+    N_t = len(u) - 1
+    deriv = np.zeros(N_t + 1)
+    for n in range(1, N_t):
+        deriv[n] = (u[n + 1] - u[n - 1]) / (2 * dt)
+    deriv[0] = (u[1] - u[0]) / dt
+    deriv[N_t] = (u[N_t] - u[N_t - 1]) / dt
+    return deriv
 
 def differentiate_vector(u, dt):
-    pass
+    N_t = len(u) - 1
+    deriv = np.zeros(N_t + 1)
+    deriv[1:N_t] = (1 / (2 * dt)) * (u[2:N_t + 1] - u[0:N_t - 1])
+    deriv[0] = (u[1] - u[0]) / dt
+    deriv[N_t] = (u[N_t] - u[N_t - 1]) / dt
+    return deriv
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
